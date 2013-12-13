0ec183fea2c78b1df9b252ffc6ccab42b323dd8f
ithinkthatmaybe <ithinkthatmaybe@gmail.com>
2013-12-13 16:46:19 +1300

    yes yes, go on

--- PYphoto.py
+++ PYphoto.py
@@ -6,13 +6,29 @@ import sys
 ##print "I am running, HAHAHAHA!"
 ##print "Arguments:", str(sys.argv)
 
-def rip(,dir_current , filter_HDR = false):
+global home = 'C:/users/sam/my photos/'
+global target = 'C:/users/sam/my photos/'
+
+def rip(rip_dir ,path = dir_current , filter_HDR = false):
     #Rip photos
     return
 
 
-def HDR_filter(path = dir_current, window = 1):
+def HDR_filter(path = dir_current, time_window = 2, seperate = false,):
+    
     return
+
+def set_dir(path, type = 'home'):
+    if type == 'home':
+        home = path
+    elif type == 'target':
+        target = path
+    else:
+        print "type not recognised, path unchanged"
+
+def dir_scan(path = home):
+    print "Path = ", path
+    
 
 
 arg1 = str(sys.argv[1])
