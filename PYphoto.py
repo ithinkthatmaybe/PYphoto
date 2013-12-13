b6bac23fe929c2256823a88c16d3af4c7202ed5b
ithinkthatmaybe <ithinkthatmaybe@gmail.com>
2013-12-13 15:52:15 +1300

    Hello there!

---
+++ PYphoto.py
@@ -0,0 +1,25 @@
+from PIL import Image
+import shutil
+import os
+import sys
+
+##print "I am running, HAHAHAHA!"
+##print "Arguments:", str(sys.argv)
+
+def rip(,dir_current , filter_HDR = false):
+    #Rip photos
+    return
+
+
+def HDR_filter(path = dir_current, window = 1):
+    return
+
+
+arg1 = str(sys.argv[1])
+print "Arg: ", arg1
+
+if arg1 == '-r':
+    print "we are ripping from the camera"
+
+elif arg1  == '-m':
+    print "we are managing"
---
+++ README.md
@@ -0,0 +1,13 @@
+#PYphoto - I you organise that shithole you call a photo library
+------------
+
+
+
+##HDR filter
+
+looks at time differences and Exposure Bias in EXIF data to determine if photos are HDR
+Moves collections of photos that look like HDR to a folder Called HDR_source
+
+
+##Panorama filter
+
